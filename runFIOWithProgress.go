625042a6f6e0ee9510d160aae7932f80bbc5b16a
TchomboW <79507838+TchomboW@users.noreply.github.com>
2024-09-20 16:27:48 +0800

    adding fio parameters

--- runFIOWithProgress.go
+++ runFIOWithProgress.go
@@ -3,14 +3,23 @@ package nrunFIOWithProgress
 import (
 	"log"
 	"os"
+	"os/exec"
 )
 
 func runFIOWithProgress(executable string, params []string) {
+	// Check if fio is installed
+	_, err := exec.LookPath(executable)
+	if err != nil {
+		log.Printf("FIO executable not found: %v", err)
+		return
+	}
+
+	// Run fio command with params
 	cmd := exec.Command(executable, params...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		log.Printf("Error running FIO: %v", err)
 	}
